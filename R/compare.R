22fd48e48f10e01c4035a47a436cbca6572e0534
Bill Behrman <behrman@stanford.edu>
2018-05-10 14:57:55 -0700

    Improve documentation

--- R/compare.R
+++ R/compare.R
@@ -11,6 +11,7 @@
 #' @examples
 #' # Ignore spec attribute created by readr functions
 #' compare(answers$q1, q1, ignore_attr = "spec")
+#'
 compare <- function(model, comparison, ignore_attr = NULL, ...) {
   for (i in seq_along(ignore_attr)) {
     attr(model, ignore[[i]]) <- NULL
