8f48fd7e7b23fba7fcecbc5f378b1b68d112c99c
Bill Behrman <behrman@stanford.edu>
2018-05-11 12:37:30 -0700

    Remove tests directory

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(dcl)
-
-test_check("dcl")
--- tests/testthat/test-compare.R
+++
@@ -1,9 +0,0 @@
-context("test-compare.R")
-
-test_that("Ignore spec attribute", {
-  df1 <- data.frame(x = 1, y = 2)
-  df2 <- data.frame(x = 1, y = 2)
-  attr(df2, "spec") <- "blah"
-
-  expect_true(compare(df1, df2, ignore_attr = "spec"))
-})
