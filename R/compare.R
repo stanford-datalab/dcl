e1060f4cde62f9cb4793ff51c52230d34775eb52
Bill Behrman <behrman@stanford.edu>
2018-05-12 13:47:44 -0700

    Check ignore_attrs argument

--- R/compare.R
+++ R/compare.R
@@ -5,16 +5,22 @@
 #'
 #' @param model        The "correct" object.
 #' @param comparison   The object to be compared with `model`.
-#' @param ignore_attrs String or list of strings specifying attributes to ignore.
+#' @param ignore_attrs String or vector of strings specifying attributes to ignore.
 #' @param ...          Arguments to be passed to `compare::compare()`.
 #'
 #' @export
 #'
 #' @examples
-#' # Ignore spec attribute created by readr functions
+#' # Ignore `spec` attribute
 #' compare(answers$q1, q1, ignore_attrs = "spec")
 #'
+#' # Ignore `spec` and `problems` attributes
+#' compare(answers$q1, q1, ignore_attrs = c("spec", "problems"))
+#'
 compare <- function(model, comparison, ignore_attrs = NULL, ...) {
+  if (!is.null(ignore_attrs) && !is.character(unlist(ignore_attrs))) {
+    stop("ignore_attrs must be NULL, a string, or a vector or list of strings")
+  }
   for (attr in ignore_attrs) {
     attr(model, attr) <- NULL
     attr(comparison, attr) <- NULL
