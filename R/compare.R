dc13e4b21cf32f44788bc080a908ace3bd45b3e1
Bill Behrman <behrman@stanford.edu>
2018-05-19 10:37:40 -0700

    Ignore spec by default, add tests

--- R/compare.R
+++ R/compare.R
@@ -1,25 +1,35 @@
 #' Compare two objects
 #'
-#' Compare two objects using [compare::compare()], ignoring attributes in
-#' `ignore_attrs`.
+#' Compare two objects using [compare::compare()], ignoring `spec` attribute by
+#' default.
 #'
 #' @param model        The "correct" object.
 #' @param comparison   The object to be compared with `model`.
 #' @param ignore_attrs String or vector of strings of attributes to ignore.
+#' Default is to ignore `spec` attribute. Use `NULL` to not ignore attributes.
 #' @param ...          Arguments to be passed to `compare::compare()`.
 #'
 #' @export
 #'
 #' @examples
-#' # Ignore `spec` attribute
-#' compare(answers$q1, q1, ignore_attrs = "spec")
+#' df1 <- data.frame(x = 1, y = 2)
+#' df2 <- df1
+#' attr(df2, "spec") <- "a"
+#' df3 <- df2
+#' attr(df3, "problems") <- "b"
 #'
-#' # Ignore `spec` and `problems` attributes
-#' compare(answers$q1, q1, ignore_attrs = c("spec", "problems"))
+#' # Ignore `spec` attribute by default
+#' compare(df1, df2)
 #'
-compare <- function(model, comparison, ignore_attrs = NULL, ...) {
+#' # Do not ignore attributes
+#' compare(df1, df2, ignore_attrs = NULL)
+#'
+#' Ignore `spec` and `problems` attributes
+#' compare(df1, df3, ignore_attrs = c("spec", "problems"))
+#'
+compare <- function(model, comparison, ignore_attrs = "spec", ...) {
   if (!is.null(ignore_attrs) && !is.character(unlist(ignore_attrs))) {
-    stop("ignore_attrs must be NULL, a string, or a vector or list of strings")
+    stop("ignore_attrs must be NULL, a string, or a vector of strings")
   }
   for (attr in ignore_attrs) {
     attr(model, attr) <- NULL
--- tests/testthat/test-compare.R
+++ tests/testthat/test-compare.R
@@ -1,10 +1,42 @@
 context("test-compare.R")
 
+test_that("Identical objects", {
+  df1 <- data.frame(x = 1, y = 2)
+  df2 <- df1
+
+  expect_true(compare::isTRUE(compare(df1, df2)))
+})
+
+test_that("Different objects", {
+  df1 <- data.frame(x = 1, y = 2)
+  df2 <- data.frame(x = 2, y = 1)
+
+  expect_false(compare::isTRUE(compare(df1, df2)))
+})
+
 test_that("Ignore spec attribute", {
   df1 <- data.frame(x = 1, y = 2)
-  df2 <- data.frame(x = 1, y = 2)
-  attr(df2, "spec") <- list("blah")
+  df2 <- df1
+  attr(df2, "spec") <- "a"
 
-  cmp <- compare(df1, df2, ignore_attrs = "spec")
-  expect_true(cmp$result)
+  expect_true(compare::isTRUE(compare(df1, df2)))
+})
+
+test_that("Do not ignore attributes", {
+  df1 <- data.frame(x = 1, y = 2)
+  df2 <- df1
+  attr(df2, "spec") <- "a"
+
+  expect_false(compare::isTRUE(compare(df1, df2, ignore_attrs = NULL)))
+})
+
+test_that("Ignore spec and problem attributes", {
+  df1 <- data.frame(x = 1, y = 2)
+  attr(df1, "spec") <- "a"
+  df2 <- df1
+  attr(df2, "problems") <- "b"
+
+  expect_true(
+    compare::isTRUE(compare(df1, df2, ignore_attrs = c("spec", "problems")))
+  )
 })
