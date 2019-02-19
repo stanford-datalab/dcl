7f2df439d697c450a0e36b6edae92261564ee92b
Bill Behrman <behrman@stanford.edu>
2019-02-18 21:33:34 -0800

    Ignore spec_tbl_df class

--- R/compare.R
+++ R/compare.R
@@ -1,12 +1,14 @@
 #' Compare two objects
 #'
-#' Compare two objects using [compare::compare()], ignoring `spec` attribute by
-#' default.
+#' Compare two objects using [compare::compare()], ignoring `spec` attribute and
+#' `spec_tbl_df` class by default.
 #'
 #' @param model        The "correct" object.
 #' @param comparison   The object to be compared with `model`.
 #' @param ignore_attrs String or vector of strings of attributes to ignore.
 #' Default is to ignore `spec` attribute. Use `NULL` to not ignore attributes.
+#' @param ignore_classes String or vector of strings of classes to ignore.
+#' Default is to ignore `spec_tbl_df` class. Use `NULL` to not ignore classes.
 #' @param ...          Arguments to be passed to `compare::compare()`.
 #'
 #' @export
@@ -15,14 +17,15 @@
 #' df1 <- data.frame(x = 1, y = 2)
 #' df2 <- df1
 #' attr(df2, "spec") <- "a"
+#' attr(df2, "class") <- c("spec_tbl_df", attr(df1, "class"))
 #' df3 <- df2
 #' attr(df3, "problems") <- "b"
 #'
-#' # Ignore spec attribute by default
+#' # Ignore spec attribute and spec_tbl_df class by default
 #' compare(df1, df2)
 #'
-#' # Do not ignore attributes
-#' compare(df1, df2, ignore_attrs = NULL)
+#' # Do not ignore attributes or classes
+#' compare(df1, df2, ignore_attrs = NULL, ignore_classes = NULL)
 #'
 #' # Ignore problems attribute
 #' compare(df2, df3, ignore_attrs = "problems")
@@ -30,13 +33,21 @@
 #' # Ignore spec and problems attributes
 #' compare(df1, df3, ignore_attrs = c("spec", "problems"))
 #'
-compare <- function(model, comparison, ignore_attrs = "spec", ...) {
+compare <- function(
+  model, comparison, ignore_attrs = "spec", ignore_classes = "spec_tbl_df", ...
+) {
   if (!is.null(ignore_attrs) && !is.character(unlist(ignore_attrs))) {
     stop("ignore_attrs must be NULL, a string, or a vector of strings")
+  }
+  if (!is.null(ignore_classes) && !is.character(unlist(ignore_classes))) {
+    stop("ignore_classes must be NULL, a string, or a vector of strings")
   }
   for (attr in ignore_attrs) {
     attr(model, attr) <- NULL
     attr(comparison, attr) <- NULL
   }
+  attr(model, "class") <- dplyr::setdiff(attr(model, "class"), ignore_classes)
+  attr(comparison, "class") <-
+    dplyr::setdiff(attr(comparison, "class"), ignore_classes)
   compare::compare(model, comparison, ...)
 }
--- tests/testthat/test-compare.R
+++ tests/testthat/test-compare.R
@@ -30,7 +30,7 @@ test_that("Do not ignore attributes", {
   expect_false(compare::isTRUE(compare(df1, df2, ignore_attrs = NULL)))
 })
 
-test_that("Ignore problem attribute", {
+test_that("Ignore problems attribute", {
   df1 <- data.frame(x = 1, y = 2)
   attr(df1, "spec") <- "a"
   df2 <- df1
@@ -39,7 +39,7 @@ test_that("Ignore problem attribute", {
   expect_true(compare::isTRUE(compare(df1, df2, ignore_attrs = "problems")))
 })
 
-test_that("Ignore spec and problem attributes", {
+test_that("Ignore spec and problems attributes", {
   df1 <- data.frame(x = 1, y = 2)
   df2 <- df1
   attr(df2, "spec") <- "a"
@@ -47,5 +47,42 @@ test_that("Ignore spec and problem attributes", {
 
   expect_true(
     compare::isTRUE(compare(df1, df2, ignore_attrs = c("spec", "problems")))
+  )
+})
+
+test_that("Ignore spec_tbl_df class", {
+  df1 <- data.frame(x = 1, y = 2)
+  df2 <- df1
+  attr(df2, "class") <- c("spec_tbl_df", attr(df2, "class"))
+
+  expect_true(compare::isTRUE(compare(df1, df2)))
+})
+
+test_that("Do not ignore classes", {
+  df1 <- data.frame(x = 1, y = 2)
+  df2 <- df1
+  attr(df2, "class") <- c("spec_tbl_df", attr(df2, "class"))
+
+  expect_false(compare::isTRUE(compare(df1, df2, ignore_classes = NULL)))
+})
+
+test_that("Ignore problems class", {
+  df1 <- data.frame(x = 1, y = 2)
+  attr(df1, "class") <- c("spec_tbl_df", attr(df1, "class"))
+  df2 <- df1
+  attr(df2, "class") <- c("problems", attr(df2, "class"))
+
+  expect_true(compare::isTRUE(compare(df1, df2, ignore_classes = "problems")))
+})
+
+test_that("Ignore spec and problems classes", {
+  df1 <- data.frame(x = 1, y = 2)
+  df2 <- df1
+  attr(df2, "class") <- c("spec_tbl_df", "problems", attr(df2, "class"))
+
+  expect_true(
+    compare::isTRUE(
+      compare(df1, df2, ignore_classes = c("spec_tbl_df", "problems"))
+    )
   )
 })
