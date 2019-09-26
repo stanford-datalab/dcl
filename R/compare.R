cd4ca0980d34d08af3e95ee5d609de216a64fdf4
Bill Behrman <behrman@stanford.edu>
2019-09-26 12:32:19 -0700

    Add ignore_component_attrs arg to compare()

--- R/compare.R
+++ R/compare.R
@@ -3,14 +3,15 @@
 #' Compare two objects using [compare::compare()], comparing only the attributes
 #' in `compare_attrs` and ignoring classes in `ignore_classes`.
 #'
-#' @param model          The "correct" object.
-#' @param comparison     The object to be compared with `model`.
+#' @param model  The "correct" object.
+#' @param comparison  The object to be compared with `model`.
 #' @param compare_attrs  String or vector of strings of attributes to compare.
 #' Default is to compare only `class`, `groups`, `names`, and `row.names`
 #' attributes. Use `NULL` to compare all attributes.
-#' @param ignore_classes String or vector of strings of classes to ignore.
+#' @param ignore_classes  String or vector of strings of classes to ignore.
 #' Default is to ignore `spec_tbl_df` class. Use `NULL` to not ignore classes.
-#' @param ...            Arguments to be passed to `compare::compare()`.
+#' @param ignore_component_attrs  Ignore attributes of object components.
+#' @param ...  Arguments to be passed to `compare::compare()`.
 #'
 #' @export
 #'
@@ -25,7 +26,7 @@
 #'
 compare <- function(
   model, comparison, compare_attrs = c("class", "names", "groups", "row.names"),
-  ignore_classes = "spec_tbl_df", ...
+  ignore_classes = "spec_tbl_df", ignore_component_attrs = TRUE, ...
 ) {
   if (!is.null(compare_attrs) && !is.character(unlist(compare_attrs))) {
     stop("compare_attrs must be NULL, a string, or a vector of strings")
@@ -42,6 +43,14 @@ compare <- function(
     attrs <- attributes(comparison)
     attributes(comparison) <-
       attrs[sort(intersect(names(attrs), compare_attrs))]
+  }
+  if (ignore_component_attrs) {
+    for (name in names(model)) {
+      attributes(model[[name]]) <- NULL
+    }
+    for (name in names(comparison)) {
+      attributes(comparison[[name]]) <- NULL
+    }
   }
   compare::compare(model, comparison, ...)
 }
