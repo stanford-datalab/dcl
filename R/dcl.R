b5408e47f1dd0f76d85c07838da019f16912caeb
skaltman <skaltman@stanford.edu>
2020-01-13 12:18:43 -0800

    fix

--- R/dcl.R
+++ R/dcl.R
@@ -8,7 +8,7 @@
 #' @importFrom dplyr %>% setdiff union
 #' @importFrom stringr str_glue str_remove
 #' @importFrom tibble glimpse
-#' @importFrom dir_create dir_exists dir_ls file_exists file_temp fs is_dir path path_common path_dir path_expand path_file
+#' @importFrom fs dir_create dir_exists dir_ls file_exists file_temp is_dir path path_common path_dir path_expand path_file
 #' @importFrom usethis proj_set proj_get proj_activate use_rstudio ui_path ui_done
 #' @importFrom purrr walk
 NULL
